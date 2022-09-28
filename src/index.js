3eafc89d83d12232040f3825488cf765a28a85c2
nedagholami <nedagholami1998@gmail.com>
2022-09-28 10:52:29 +0330

    added new files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import "./i18nextInit";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+reportWebVitals();
